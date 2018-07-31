60318c0795861509c1f5b27c2c3f55a908571539
Kodiak Conrad <kconrad1@uw.edu>
2018-07-31 09:34:14 -0700

    successfully grabs data from cloudian

--- test.py
+++ test.py
@@ -3,32 +3,14 @@
 #Avro Grapher
 
 import boto
+from boto.s3.bucket import Bucket
+from boto.s3.key import Key
 import traceback
 
 
 bucket_name = "iot-data"
 my_topic = "avro-demo"
 flush_size = 3
-
-
-try:
-	conn = boto.connect_s3(host = 'http://tims4.mobi-cloud.com:80', 
-		aws_access_key_id = "00c36f16c2600f70ae60",
-		aws_secret_access_key = "XsSbmCIfcYrX5NdCBj7n1QSaU2lhdgDJJBDlT7VE",
-		is_secure = False)                                                                                                                                                       
-	print conn
-	print("connection made")
-	bucket = conn.get_bucket(bucket_name)
-	print(bucket)
-	bucket.download_file(get_key_name(), get_key_name())
-	print("downloaded")
-except Exception as e:
-	print(e)
-	traceback.print_exc()
-#gkey = Key(bucket=bucket, name=get_key_name())
-
-offset = 0
-prefix = "topics/"+my_topic+"/partition=0/"+my_topic+"+0+"
 
 def get_key_name():
 	'''
@@ -37,3 +19,23 @@ def get_key_name():
 	offset = offset + flush_size
 	return my_key'''
 	return "topics/avro-demo/partition=0/avro-demo+0+0000000000.avro"
+
+try:
+	conn = boto.connect_s3(host = 'tims4.mobi-cloud.com', port=80, is_secure = False)                                                                                                                                                       
+	print conn
+	print("connection made")
+	bucket = Bucket(conn, bucket_name)
+	#bucket = conn.get_bucket(bucket_name)
+	#print(bucket)
+	gkey = Key(bucket=bucket, name=get_key_name())
+	#bucket.download_file(get_key_name(), get_key_name())
+	print(gkey.get_contents_as_string())
+	print("downloaded")
+except Exception as e:
+	print(e)
+	traceback.print_exc()
+
+
+#offset = 0
+#prefix = "topics/"+my_topic+"/partition=0/"+my_topic+"+0+"
+
